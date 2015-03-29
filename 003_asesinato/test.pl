b6ca91e2478ef0b2eb0339cc57a9f837a907a3fa
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-29 20:58:49 -0300

    Agrego mensaje con respuesta (no sé si va a andar)

--- 003_asesinato/test.pl
+++ 003_asesinato/test.pl
@@ -5,4 +5,6 @@ test(carnicero_no_mato_a_agatha):-
 	not(asesino(agatha, carnicero)).
 
 test(charles_no_mato_a_agatha):-
-	not(asesino(agatha, charles)).
+	not(asesino(agatha, charles)),imprimir("\nMensaje: Por si te quedo la duda, Agatha se suicido\n").
+
+imprimir(Mensaje):-atom_string(Mensaje,X),write(X).
