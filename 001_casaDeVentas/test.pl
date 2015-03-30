6aa637fc6c4c1b661c0295e523f9105c14a34eda
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-29 22:38:19 -0300

    Arreglo los tests para que resuelvan la expresión

--- 001_casaDeVentas/test.pl
+++ 001_casaDeVentas/test.pl
@@ -1,27 +1,27 @@
-test(precio_agua_particular):-
-	calcularPrecio(luisa, villavicencio, 2*1.3).
-test(precio_gaseosa_particular):-
-	calcularPrecio(luisa, cocacola, 4*1.4).
-test(precio_vino_nacional_particular):-
-	calcularPrecio(luisa, bianchi, 7*1.6).
-test(precio_wiskey_nacional_particular):-
-	calcularPrecio(luisa, criadores, 20*1.6).
-test(precio_vino_importado_particular):-
-	calcularPrecio(luisa, cucagna, 18*1.7).
-test(precio_wiskey_importado_particular):-
-	calcularPrecio(luisa, grants, 30*1.7).
+test(precio_agua_particular,nondet):-
+	calcularPrecio(luisa, villavicencio,X),X is 2*1.3.
+test(precio_gaseosa_particular,nondet):-
+	calcularPrecio(luisa, cocacola, X),X is 4*1.4.
+test(precio_vino_nacional_particular,nondet):-
+	calcularPrecio(luisa, bianchi,X),X is 7*1.6.
+test(precio_wiskey_nacional_particular,nondet):-
+	calcularPrecio(luisa, criadores,X),X is 20*1.6.
+test(precio_vino_importado_particular,nondet):-
+	calcularPrecio(luisa, cucagna,X),X is 18*1.7.
+test(precio_wiskey_importado_particular,nondet):-
+	calcularPrecio(luisa, grants,X),X is 30*1.7.
 
-test(precio_agua_comerciante):-
-	calcularPrecio(zoraida, villavicencio, 2*1.25).
-test(precio_gaseosa_comerciante):-
-	calcularPrecio(zoraida, cocacola, 4*1.25).
-test(precio_vino_nacional_comerciante):-
-	calcularPrecio(zoraida, bianchi, 7*1.3 + 1).
-test(precio_vino_importado_mas_3_comerciante):-
-	calcularPrecio(zoraida, richelieu, 13 + 3).
-test(precio_vino_importado_con_porcentaje_comerciante):-
-	calcularPrecio(zoraida, cucagna, 18*1.2).
-test(precio_wiskey_nacional_comerciante):-
-	calcularPrecio(zoraida, criadores, 20*1.5).
-test(precio_wiskey_importado_comerciante):-
-	calcularPrecio(zoraida, grants, 30*1.5).
+test(precio_agua_comerciante,nondet):-
+	calcularPrecio(zoraida, villavicencio,X),X is 2*1.25.
+test(precio_gaseosa_comerciante,nondet):-
+	calcularPrecio(zoraida, cocacola,X),X is 4*1.25.
+test(precio_vino_nacional_comerciante,nondet):-
+	calcularPrecio(zoraida, bianchi,X),X is 7*1.3+1.
+test(precio_vino_importado_mas_3_comerciante,nondet):-
+	calcularPrecio(zoraida, richelieu,X),X is 13 + 3.
+test(precio_vino_importado_con_porcentaje_comerciante,nondet):-
+	calcularPrecio(zoraida, cucagna,X),X is 18*1.2.
+test(precio_wiskey_nacional_comerciante,nondet):-
+	calcularPrecio(zoraida, criadores,X),X is 20*1.5.
+test(precio_wiskey_importado_comerciante,nondet):-
+	calcularPrecio(zoraida, grants,X),X is 30*1.5.
