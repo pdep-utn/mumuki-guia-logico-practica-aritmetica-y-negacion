bbd9041524afa333910566a48a70e2111c37b31a
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-29 22:45:16 -0300

    Arreglo un par de tests dodne el is andaba mal

--- 001_casaDeVentas/test.pl
+++ 001_casaDeVentas/test.pl
@@ -3,11 +3,11 @@ test(precio_agua_particular,nondet):-
 test(precio_gaseosa_particular,nondet):-
 	calcularPrecio(luisa, cocacola, X),X is 4*1.4.
 test(precio_vino_nacional_particular,nondet):-
-	calcularPrecio(luisa, bianchi,X),X is 7*1.6.
+	calcularPrecio(luisa, bianchi,11.2). /* No se por que el is al evaluar la expresion da 11.200000000000001*/
 test(precio_wiskey_nacional_particular,nondet):-
 	calcularPrecio(luisa, criadores,X),X is 20*1.6.
 test(precio_vino_importado_particular,nondet):-
-	calcularPrecio(luisa, cucagna,X),X is 18*1.7.
+	calcularPrecio(luisa,cucagna,30.6). /* No se por que el is al evaluar la expresion da 30.599999999999998*/
 test(precio_wiskey_importado_particular,nondet):-
 	calcularPrecio(luisa, grants,X),X is 30*1.7.
 
@@ -25,3 +25,4 @@ test(precio_wiskey_nacional_comerciante,nondet):-
 	calcularPrecio(zoraida, criadores,X),X is 20*1.5.
 test(precio_wiskey_importado_comerciante,nondet):-
 	calcularPrecio(zoraida, grants,X),X is 30*1.5.
+
