2f768699fbffdb4b968186ba27f51d60a5fa0224
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-30 00:12:23 -0300

    Agrego base de conocimiento

--- 002_competenciaDeHabilidades/test.pl
+++ 002_competenciaDeHabilidades/test.pl
@@ -1,3 +1,20 @@
+metrosLanzamiento(ricky,10).
+metrosLanzamiento(lowy,8).
+metrosLanzamiento(gonza,13).
+metrosLanzamiento(jess,6).
+metrosLanzamiento(roli,17).
+
+metrosMartillo(ricky,12).
+metrosMartillo(lowy,6).
+metrosMartillo(gonza,9).
+metrosMartillo(jess,4).
+metrosMartillo(roli,1).
+
+metrosEscoba(ricky,13).
+metrosEscoba(lowy,7).
+metrosEscoba(gonza,10).
+metrosEscoba(jess,4).
+metrosEscoba(roli,4).
 
 test(ricky_saco_10_en_lanzamiento,nondet):-
 	puntajeLanzamientoPrecision(ricky,10).
