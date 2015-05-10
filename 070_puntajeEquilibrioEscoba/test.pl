7c7a3283df804efec243e70c429a4795a71f806a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-10 17:05:46 -0300

    Separando ejercicio 2 en 3

---
+++ 070_puntajeEquilibrioEscoba/test.pl
@@ -0,0 +1,14 @@
+test(ricky_saco_4_en_escoba,nondet):-
+	puntajeEquilibrioEscoba(ricky,4).
+
+test(gonza_saco_3_en_escoba,nondet):-
+	puntajeEquilibrioEscoba(gonza,3).
+
+test(lowy_saco_2_en_escoba,nondet):-
+	puntajeEquilibrioEscoba(lowy,2).
+
+test(jess_saco_1_en_escoba,nondet):-
+	puntajeEquilibrioEscoba(jess,1).
+
+test(roli_saco_1_en_escoba,nondet):-
+	puntajeEquilibrioEscoba(roli,1).
