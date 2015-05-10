e846f651690ec12c92121e09455b555400f0dcf9
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-10 15:55:22 -0300

    typo

--- 050_puntajeTotal/test.pl
+++ 050_puntajeTotal/test.pl
@@ -8,5 +8,5 @@ test(si_escoba_no_llega_a_5_es_cero, nondet) :-
   puntajeTotal(hector, 0).
 
 test(si_todos_superan_5_es_su_sumatoria) :-
-  puntajeTotal(brienne, X).
+  puntajeTotal(brienne, X),
   assertion(X == 60).
