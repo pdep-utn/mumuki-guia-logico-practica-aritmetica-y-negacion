66cc4b6e00c991b9cf87020d06b607665365222b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-07 20:16:59 -0300

    Corrigiendo test

--- 001_calcularPrecio/test.pl
+++ 001_calcularPrecio/test.pl
@@ -18,21 +18,21 @@ test('si una alcoholica importada tiene costo 30, el precio particular es 51',no
   Y is 30*1.7,
   assertion(X == Y).
 
-test('si el costo de agua es 2, el costo comerciante es 2.5',nondet):-
+test('si el costo de un agua es 2, el costo comerciante es 2.5',nondet):-
   calcularPrecio(zoraida, villavicencio,X),
   Y is 2*1.25, 
   assertion(X == Y).
 
-test('si el costo de gaseosa es 4, el precio comerciante es 4',nondet):-
+test('si el costo de una gaseosa es 4, el precio comerciante es 4',nondet):-
   calcularPrecio(zoraida, cocacola,X),
   assertion(X == 4).
 
-test('si el costo de alcoholica nacional es 20, el precio comerciante es 26',nondet):-
+test('si el costo de una alcoholica nacional es 20, el precio comerciante es 26',nondet):-
   calcularPrecio(zoraida, criadores, X),
-  X is 20 * 1.3,
+  Y is 20 * 1.3,
   assertion(X == Y).
 
-test('si el costo de alcoholica importada es 30, el precio comerciante es 39',nondet):-
+test('si el costo de una alcoholica importada es 30, el precio comerciante es 39',nondet):-
   calcularPrecio(zoraida, grants,X),
   Y is 30 * 1.3,
   assertion(X == Y).
