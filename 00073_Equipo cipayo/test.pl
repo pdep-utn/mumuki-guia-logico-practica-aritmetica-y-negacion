bf0a3795df6768284456e63c4787e76f791b2ff9
est.miguel@gmail.com <est.miguel@gmail.com>
2016-05-09 12:56:50 -0400

    Mumuki Export on 2016-05-09 12:56:50 -0400

--- 00073_Equipo cipayo/test.pl
+++ 00073_Equipo cipayo/test.pl
@@ -2,7 +2,7 @@ test('si dos miembros pertencen a otro equipo, es cipayo'):-
   equipoCipayo(d).
   
 test('si ningun miembro pertence a otro equipo, no es cipayo'):-
-  equipoCipayo(a).
+  \+ equipoCipayo(a).
   
 test('si un sólo miebro pertence a otro equipo, no es cipayo'):-
-  equipoCipayo(b).
+  \+ equipoCipayo(b).
