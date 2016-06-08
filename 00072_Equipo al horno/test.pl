8cca55d0bea7d2c5819bb848a453014733d4cafe
mmatos87@gmail.com <mmatos87@gmail.com>
2016-06-08 10:39:10 -0400

    Mumuki Export on 2016-06-08 10:39:10 -0400

--- 00072_Equipo al horno/test.pl
+++ 00072_Equipo al horno/test.pl
@@ -1,14 +1,14 @@
-test('esta al horno si tiene un sólo integrante y está fuera de juego') :-
+test('esta al horno si tiene un solo integrante y esta fuera de juego') :-
   alHorno(a).
   
-test('esta al horno si tiene un sólo integrante y está débil') :-
+test('esta al horno si tiene un solo integrante y esta debil') :-
   alHorno(b).
   
-test('esta al horno si tiene dos integrantes y todos están débiles') :-
+test('esta al horno si tiene dos integrantes y todos estan debiles') :-
   alHorno(c).
   
-test('esta al horno si tiene dos integrantes y uno está débil y el otro fuera de juego') :-
+test('esta al horno si tiene dos integrantes y uno esta debil y el otro fuera de juego') :-
   alHorno(e).
   
-test('no esta al horno si uno de sus integrantes está en el juego no débil') :-
+test('no esta al horno si uno de sus integrantes esta en el juego no debil') :-
   \+ alHorno(d).
