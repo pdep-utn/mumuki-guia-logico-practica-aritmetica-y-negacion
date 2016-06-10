8608c66e3ad7a1f522665e04bc6ae6cee282a86d
crespi.gustavo@gmail.com <crespi.gustavo@gmail.com>
2016-06-10 08:43:48 -0400

    Mumuki Export on 2016-06-10 08:43:48 -0400

--- 00074_Equipo Abandonó/test.pl
+++ 00074_Equipo Abandonó/test.pl
@@ -9,3 +9,6 @@ test('abandono un equipo si no tiene integrantes') :-
  
 test('no abandono si no es un equipo') :-
  \+ abandono(d).
+ 
+test('abandono es inversible y se relaciona con todos los equipos que abandonaron', set(Equipo == [a])) :-
+ abandono(Equipo).
